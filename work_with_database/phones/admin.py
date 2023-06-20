19553659916d3153c4c9c0468ab5b3d56ef55868
Andrey Bogatyrev <nerou@internet.ru>
2023-06-20 20:05:46 +0300

    First task

---
+++ work_with_database/phones/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from phones.models import *
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'release_date']
+    list_editable = ['name', 'price', ]
